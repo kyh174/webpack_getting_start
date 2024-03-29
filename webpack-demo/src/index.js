6ff3020f171aed84d08c69fbedca64c8c349079d
kyh174 <kyh174@naver.com>
2024-03-29 11:33:59 +0900

    output 관리

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,17 +1,16 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './codewalks_favicon.png';
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   element.innerHTML = _.join(['Hello', 'webpack'], '');
-  element.classList.add('hello');
 
-  const myIcon = new Image();
-  myIcon.src = Icon;
+  btn.innerHTML = 'Click me and check the console';
+  btn.onclick = printMe;
 
-  element.appendChild(myIcon);
+  element.appendChild(btn);
 
   return element;
 }
